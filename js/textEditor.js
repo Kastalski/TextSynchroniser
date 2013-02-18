420d2e97e82bed1b527d3c4acfa6561c6e2ed834
kastalski <S.Kastalski@software.travel>
2013-02-18 19:11:34 +0300

    Task6

--- js/textEditor.js
+++ js/textEditor.js
@@ -8,30 +8,14 @@
 function loadTextEditor() {
     var min = 8
     var max = 20
-    var step = 1
-    var sliderValues = generateSizeValues(min, max, step)
     var options = {
-        max: max,
+        range: true,
         min: min,
-        values: sliderValues
+        max: max
     }
-    var $textSizeSlider = $('#textSizeSlider').slider()
+    var $textSizeSlider = $('#textSizeSlider').slider(options)
     $textSizeSlider.on( "slidechange", function( event, ui ) {
         console.log("sd=" + ui.value)
     })
 
-}
-
-function generateSizeValues(min, max, step) {
-    if (!isNumber(min) || !isNumber(max) || !isNumber(step)) return []
-    if (min > max) return []
-    var result = []
-    for (var i = min; i <= max; i++) {
-        result.push(i)
-    }
-    return result;
-}
-
-function isNumber(string) {
-    return !isNaN(parseFloat(string)) && isFinite(string)
 }
