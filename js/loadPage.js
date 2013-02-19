cf7eeda0ed0ce0db0c23b570e5f496bcc19e9d6c
kastalski <S.Kastalski@software.travel>
2013-02-19 16:05:19 +0300

    Task6

--- js/loadPage.js
+++ js/loadPage.js
@@ -5,7 +5,59 @@
  * Time: 18:27
  * To change this template use File | Settings | File Templates.
  */
+var serverData = {
+    'font': "Courier New",
+    'size': "12",
+    'text': "Snowboarding is a winter sport that involves descending a slope that is ..."
+}
+
+var lastText = $('textEditor').val()
+var sendParamsCall = null
+var isEdit = false
 $(document).ready(function() {
     loadTextEditor()
+    runUpdateFromServer()
 
+
+    $("#textEditor").keyup(function() {
+        var $this = $(this)
+        $('.resultTextPanel').html($this.val())
+        clearTimeout(sendParamsCall)
+        isEdit = true
+        sendParamsCall = setTimeout(function() {
+            if ($this.val() != lastText) {
+                lastText = $this.val()
+                sendData(lastText)
+                isEdit = false
+                runUpdateFromServer()
+            }
+        }, 3000)
+    })
 })
+
+function sendData(text) {
+    // send data to server
+    console.log("text:" + text)
+    console.log("font:" + $('#currentFont').html())
+    console.log("size:" + $('#sizeValue').html())
+}
+
+function runUpdateFromServer() {
+    if (isEdit) return
+    jQuery.ajax({
+        type: 'get',
+        url: 'http://blah.com',
+        dataType: 'json',
+        complete: function() {
+            if (isEdit) return
+
+            serverData.size = getRandomBeetwen(10, 18)
+            updateTextEditor(serverData)
+            getParamsCall = setTimeout(runUpdateFromServer, 5000)
+        }
+    })
+}
+
+function getRandomBeetwen(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
--- js/textEditor.js
+++ js/textEditor.js
@@ -6,16 +6,66 @@
  * To change this template use File | Settings | File Templates.
  */
 function loadTextEditor() {
-    var min = 8
-    var max = 20
+    loadSizeSlider(8, 20)
+    loadFontList()
+}
+
+function loadSizeSlider(min, max) {
     var options = {
-        range: true,
+        range: false,
         min: min,
         max: max
     }
     var $textSizeSlider = $('#textSizeSlider').slider(options)
-    $textSizeSlider.on( "slidechange", function( event, ui ) {
-        console.log("sd=" + ui.value)
+    $('#sizeValue').html(min)
+
+    setSizeToText(min)
+    $textSizeSlider.bind("slidechange", function( event, ui ) {
+        $('.resultTextPanel').css({ "font-size": ui.value + 'pt' })
+        $('#sizeValue').html(ui.value)
+    })
+}
+
+function loadFontList() {
+    var $fontsList = $('div#fonts')
+    var $chosenFont = $fontsList.find('div#currentFont')
+    var defaultFont = $fontsList.find('li:first')
+    $chosenFont.css({ "font-family": defaultFont.attr('data') }).html(defaultFont.html())
+
+    $fontsList.find('ul li:odd').css({ "background-color": "#efefef" })
+
+    $chosenFont.bind("click", function() {
+        $(this).next().slideToggle()
     })
 
+    setFontToText(defaultFont.html())
+    $fontsList.find('li').bind("click", function() {
+        var $this = $(this)
+        $this.parent().slideToggle()
+        $this.html($this.attr('data'))
+        $this.css({"font-family": $this.attr('data')})
+        setFontToText($this.attr('data'))
+    })
+}
+
+function updateTextEditor(options) {
+    // update params
+    $('#textEditor').val(options.text)
+
+    var $chosenFont = $('div#fonts div#currentFont')
+    $chosenFont.css({ "font-family": options.font })
+    $chosenFont.html(options.font)
+    // update text panel
+    $('.resultTextPanel').html(options.text)
+
+    setFontToText(options.font)
+    setSizeToText(options.size)
+}
+
+function setFontToText(font) {
+    $('.resultTextPanel').css({ "font-family": font })
+}
+
+function setSizeToText(size) {
+    $("#textSizeSlider").slider( "value", size )
 }
