398bc051820bcf295943dad96aaf9a4d8e1ec95f
kastalski <S.Kastalski@software.travel>
2013-02-20 09:55:20 +0300

    Task6

--- js/main.js
+++ js/main.js
@@ -8,12 +8,14 @@
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
-        app: '../app'
+        app: '../app',
+        'jquery': 'http://code.jquery.com/jquery-1.9.1',
+        'jqueryui': 'http://code.jquery.com/ui/1.10.1/jquery-ui'
     }
 })
 
-requirejs(['app/loadPageComponents'],
-    function (loadPageComponents) {
+requirejs(['app/loadPageComponents', 'jquery', 'jqueryui'],
+    function (loadPageComponents, jQuery) {
         Math.getRandomBeetwen = function(min, max) {
             return this.round(this.random() * (max - min) + min)
         }
